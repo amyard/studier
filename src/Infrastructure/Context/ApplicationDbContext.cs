c990c91c0cbd3d4e07f8ae496016d23e07c9c017
amyard <maksymturenko@gmail.com>
2022-06-10 08:56:50 +0300

    add sql server

---
+++ src/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        base.OnModelCreating(builder);
+    }
+}
--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,13 +1,26 @@
+using Infrastructure;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// load project dependency injections
+builder.Services.AddInfrastructure(builder.Configuration);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// RUN MIGRATIONS
+await using var scope = app.Services.CreateAsyncScope();
+await using var db = scope.ServiceProvider.GetService<ApplicationDbContext>();
+await db.Database.MigrateAsync();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
